75b966f480ca9e7dada43c7365b8eaf23350213c
Ernest V <ernest.viol@gmail.com>
2026-05-19 20:04:29 -0700

    feat: Add prismaSession to entrypoint

---
+++ README.md
@@ -0,0 +1,17 @@
+# File Uploader
+
+A google drive like application where users can upload and share files.
+
+## Architecture
+
+### Architecture Design
+
+### System Design
+
+### Database Design
+
+## Installation
+
+## Attributions
+
+CSS reset from [Josh Comeau](https://www.joshwcomeau.com/css/custom-css-reset/)
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { prismaSession } from './libs/session.js';
 
 const PORT = process.env.PORT || 3000;
 const viewsPath = path.join(
@@ -19,6 +20,8 @@ app.set('view engine', 'ejs');
 // Middlewares
 app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(prismaSession);
 
 // Automatically set currentUser for views
 app.use((req, res, next) => {
